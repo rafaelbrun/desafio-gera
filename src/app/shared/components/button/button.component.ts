061e66695f21fc5a04824f33f5be5f1738a800fe
Rafael <rafaelbrungolin@gmail.com>
2021-08-09 12:40:37 -0400

    Inclui component button e input

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() iconName: IconDefinition | undefined;
+  @Input() buttonLabel: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
